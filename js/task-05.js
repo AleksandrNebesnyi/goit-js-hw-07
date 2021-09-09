1afd99092429853dd3768ea072834de50e238f40
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-09-09 12:05:01 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,14 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+const h1El = document.querySelector("h1");
+console.log(h1El);
+console.log(inputEl);
 
-// inputEl.addEventListener("input", onInputChange);
+inputEl.addEventListener("input", onInputChange);
 
-inputEl.addEventListener("input", (event) => {
-  utputEl.textContent =
-    event.target.value === "" ? "незнакомец" : event.target.value;
-});
-
-// function onInputChange(event) {
-//   if ((outputEl.textContent = "")) {
-//     outputEl.textContent = "незнакомец";
-//   } else outputEl.textContent = event.target.value;
-// }
+function onInputChange(event) {
+  if (inputEl.value === "") {
+    h1El.textContent = "незнакомец";
+    // outputEl.textContent = "незнакомец";
+  } else outputEl.textContent = event.target.value;
+}
